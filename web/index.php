367a96c7a1c685e836548f2f0a94a8629e2cc2f4
Aprendiz <Aprendiz@CALAPRCTIFSP083.FormacionSena.RED>
2021-10-19 15:48:12 -0500

    Creacion de la estructura

---
+++ web/index.php
@@ -0,0 +1,34 @@
+<?php
+
+echo "<!DOCTYPE html>";
+echo "<html lang='en'>";
+
+// <!-- header -->
+include_once '../view/partials/header.php';
+echo "<body>";
+echo "<div class='wrapper'>";
+
+	// <!-- navbar -->
+	include_once '../view/partials/navbar.php';
+
+	// <!-- sidebar -->
+	include_once '../view/partials/sidebar.php';
+
+	// <!-- home -->
+	include_once '../view/partials/home.php';
+		
+echo "</div>";
+
+echo "</body>";
+
+
+// <!-- footer -->
+include_once '../view/partials/footer.php';
+
+
+
+echo "</html>";
+
+
+
+?>
