27328b1324b62c7f2bdddc457f440178d845f129
JairHero <jahernandez2610@misena.edu.co>
2021-10-20 04:15:31 -0500

    probando commit

--- web/index.php
+++ web/index.php
@@ -1,52 +1,51 @@
 <?php
 
+	include_once '../lib/helpers.php';
+	// include_once '../lib/helpersLogin.php';
 
-include_once '../lib/helpers.php';
-// include_once '../lib/helpersLogin.php';
+	echo "<!DOCTYPE html>";
+	echo "<html lang='en'>";
 
+	// <!-- header -->
+		include_once '../view/partials/header.php';
 
-echo "<!DOCTYPE html>";
-echo "<html lang='en'>";
+		echo "<body>";
+			echo "<div class='wrapper'>";
 
-// <!-- header -->
-include_once '../view/partials/header.php';
-echo "<body>";
-echo "<div class='wrapper'>";
+		// <!-- navbar -->
+		include_once '../view/partials/navbar.php';
 
-	// <!-- navbar -->
-	include_once '../view/partials/navbar.php';
-
-	// <!-- sidebar -->
-	include_once '../view/partials/sidebar.php';
-
-
-	echo '<div class="main-panel">';
-
-	
-	if (isset($_GET['modulo'])) {
-		resolve();
-	}else{
-		// <!-- home -->
-		include_once '../view/partials/home.php';
-	}
+		// <!-- sidebar -->
+		include_once '../view/partials/sidebar.php';
 
 
-	include_once '../view/partials/copy.php';
+		echo '<div class="main-panel">';
 
-	echo '</div>';
-	
-echo "</div>";
-
-echo "</body>";
-
-
-// <!-- footer -->
-include_once '../view/partials/footer.php';
+		
+		if (isset($_GET['modulo'])) {
+			resolve();
+		}else{
+			// <!-- home -->
+			include_once '../view/partials/home.php';
+		}
 
 
+		include_once '../view/partials/copy.php';
 
-echo "</html>";
+		echo '</div>';
+		
+	echo "</div>";
+
+	echo "</body>";
+
+
+	// <!-- footer -->
+	include_once '../view/partials/footer.php';
 
 
 
-?>
+	echo "</html>";
+
+
+
+	?>
