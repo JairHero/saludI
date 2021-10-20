a723a60b8d50f2d6c31a870e297c7581baf80228
JairHero <jahernandez2610@misena.edu.co>
2021-10-20 15:15:03 -0500

    probando el funcionamiento de tessearct

--- Controller/HistoriaClinica/HistoriaClinicaController.php
+++ Controller/HistoriaClinica/HistoriaClinicaController.php
@@ -7,6 +7,31 @@
             include_once '../view/cargaHistoriaClinica/cargaHistoriaClinica.php';
 
         }
+
+        public function convertirImagenaTexto(){
+            
+            if (!isset($_FILES["imgHistoria"])) {
+                exit("No hay imagen");
+            }
+        
+            $imagen = $_FILES["imgHistoria"];
+            $ubicacionImagen = $imagen["tmp_name"];
+            $comando = "tesseract " . escapeshellarg($ubicacionImagen) . " stdout -l spa ";
+        
+            exec($comando, $textoDetectado, $codigoSalida);
+        
+            if ($codigoSalida === 0) {
+                // Tenemos el texto como un array, podemos unirlo
+                $textoComoCadena = join("\n", $textoDetectado);
+
+                include_once '../view/cargaHistoriaClinica/cargaHistoriaClinica.php';
+
+
+            } else {
+                echo "Error detectando texto. Por favor verifique que la imagen existe y que el programa de detección está instalado y es accesible desde PHP. El código de salida es: " . $codigoSalida;
+            }
+            
+        }
     }
 
 
