6bb0df602a42d34f15b9c65e0cb39b389ef51809
Aprendiz <Aprendiz@CALAPRCTIFSP085.FormacionSena.RED>
2021-10-22 10:39:49 -0500

    commit de las firmas

--- view/Medico/insert.php
+++ view/Medico/insert.php
@@ -10,15 +10,15 @@
                         <div class="card-title">Registrar Medico</div>
                     </div>
                     <div class="card-body">
-                        <form action="" class="row">
+                        <form form id='formCanvas' method='post' action='#' ENCTYPE='multipart/form-data' class="row">
                             <div class="col-md-6 col-sm-6 form-group has-feedback">
                                 <label for="fullname">Nombre Completo<b style="color:red;">*</b></label>
-                                <input type="text" class="form-control"/>             
+                                <input type="text" class="form-control" />
                             </div>
 
                             <div class="col-md-6 col-sm-6 form-group has-feedback">
                                 <label for="fullname">Apellido Completo<b style="color:red;">*</b></label>
-                                <input type="text" class="form-control"/>             
+                                <input type="text" class="form-control" />
                             </div>
 
                             <div class="form-group col-md-6 col-sm-6">
@@ -34,22 +34,22 @@
 
                             <div class="col-md-6 col-sm-6 form-group has-feedback">
                                 <label for="fullname">Numero de documento<b style="color:red;">*</b></label>
-                                <input type="text" class="form-control"/>             
+                                <input type="text" class="form-control" />
                             </div>
 
                             <div class="col-md-6 col-sm-6 form-group has-feedback">
                                 <label for="fullname">Correo<b style="color:red;">*</b></label>
-                                <input type="email" class="form-control"/>             
+                                <input type="email" class="form-control" />
                             </div>
 
                             <div class="col-md-6 col-sm-6 form-group has-feedback">
                                 <label for="fullname">Numero celular<b style="color:red;">*</b></label>
-                                <input type="text" class="form-control"/>             
+                                <input type="text" class="form-control" />
                             </div>
 
                             <div class="col-md-6 col-sm-6 form-group has-feedback">
                                 <label for="fullname">Direccion<b style="color:red;">*</b></label>
-                                <input type="text" class="form-control"/>             
+                                <input type="text" class="form-control" />
                             </div>
 
                             <div class="form-group col-md-6 col-sm-6">
@@ -153,7 +153,7 @@
 
                             <div class="col-md-6 col-sm-6 form-group has-feedback">
                                 <label for="fullname">Fecha de Nacimiento<b style="color:red;">*</b></label>
-                                <input type="date" class="form-control"/>             
+                                <input type="date" class="form-control" />
                             </div>
 
                             <div class="form-group col-md-6 col-sm-6">
@@ -176,6 +176,24 @@
                                     <option>4</option>
                                     <option>5</option>
                                 </select>
+                            </div>
+
+                            <div class="col-md-12 col-sm-6 form-group has-feedback">
+                                <div class="row">
+                                <label for="fullname">Firma<b style="color:red;">*</b></label>
+                                </div>
+                                
+                                <canvas id='canvas' width="300" height="300" style='border: 1px solid #CCC;'>
+                                    <p>Tu navegador no soporta canvas</p>
+                                </canvas>
+                                <button type='button' onclick='LimpiarTrazado()'>Borrar</button>
+                                <button type='button' onclick='GuardarTrazado()'>Guardar</button>
+                                <input type='hidden' name='imagen1' id='imagen1' />
+                                <input type='hidden' name='imagen2' id='imagen2' />
+                                <input type='hidden' name='imagen3' id='imagen3' />
+                                <img id="img1" src="" height="100px" width="100px" style="border: solid 1px;">
+                                <img id="img2" src="" height="100px" width="100px" style="border: solid 1px;">
+                                <img id="img3" src="" height="100px" width="100px" style="border: solid 1px;">
                             </div>
                         </form>
                     </div>
@@ -188,3 +206,191 @@
         </div>
     </div>
 </div>
+
+<script>
+    /* Variables de Configuracion */
+var idCanvas='canvas';
+var idForm='formCanvas';
+var inputImagen='imagen1';
+var inputImagen2='imagen2';
+var inputImagen3='imagen3';
+var estiloDelCursor='crosshair';
+var colorDelTrazo='#555';
+var colorDeFondo='#fff';
+var grosorDelTrazo=2;
+
+let aux;
+
+/* Variables necesarias */
+var contexto=null;
+var valX=0;
+var valY=0;
+var flag=false;
+var imagen1=document.getElementById(inputImagen); 
+var imagen2=document.getElementById(inputImagen2);
+var imagen3=document.getElementById(inputImagen3);
+var anchoCanvas=document.getElementById(idCanvas).offsetWidth;
+var altoCanvas=document.getElementById(idCanvas).offsetHeight;
+var pizarraCanvas=document.getElementById(idCanvas);
+
+/* Esperamos el evento load */
+window.addEventListener('load',IniciarDibujo,false);
+
+function IniciarDibujo(){
+  /* Creamos la pizarra */
+  pizarraCanvas.style.cursor=estiloDelCursor;
+  contexto=pizarraCanvas.getContext('2d');
+  contexto.fillStyle=colorDeFondo;
+  contexto.fillRect(0,0,anchoCanvas,altoCanvas);
+  contexto.strokeStyle=colorDelTrazo;
+  contexto.lineWidth=grosorDelTrazo;
+  contexto.lineJoin='round';
+  contexto.lineCap='round';
+  /* Capturamos los diferentes eventos */
+  pizarraCanvas.addEventListener('mousedown',MouseDown,false);// Click pc
+  pizarraCanvas.addEventListener('mouseup',MouseUp,false);// fin click pc
+  pizarraCanvas.addEventListener('mousemove',MouseMove,false);// arrastrar pc
+
+  pizarraCanvas.addEventListener('touchstart',TouchStart,false);// tocar pantalla tactil
+  pizarraCanvas.addEventListener('touchmove',TouchMove,false);// arrastras pantalla tactil
+  pizarraCanvas.addEventListener('touchend',TouchEnd,false);// fin tocar pantalla dentro de la pizarra
+  pizarraCanvas.addEventListener('touchleave',TouchEnd,false);// fin tocar pantalla fuera de la pizarra
+}
+
+function prueba(e){
+  console.log(e);
+}
+
+function MouseDown(e){
+  flag=true;
+  contexto.beginPath();
+  valX=e.pageX-posicionX(pizarraCanvas); valY=e.pageY-posicionY(pizarraCanvas);
+  contexto.moveTo(valX,valY);
+}
+
+function MouseUp(e){
+  contexto.closePath();
+  flag=false;
+}
+
+function MouseMove(e){
+  if(flag){
+    contexto.beginPath();
+    contexto.moveTo(valX,valY);
+    valX=e.pageX-posicionX(pizarraCanvas); valY=e.pageY-posicionY(pizarraCanvas);
+    contexto.lineTo(valX,valY);
+    contexto.closePath();
+    contexto.stroke();
+  }
+}
+
+function TouchMove(e){
+  e.preventDefault();
+  if (e.targetTouches.length == 1) { 
+    var touch = e.targetTouches[0]; 
+    MouseMove(touch);
+  }
+}
+
+function TouchStart(e){
+  if (e.targetTouches.length == 1) { 
+    var touch = e.targetTouches[0]; 
+    MouseDown(touch);
+  }
+}
+
+function TouchEnd(e){
+  if (e.targetTouches.length == 0) { 
+    var touch = e.targetTouches[0]; 
+    console.log("aaaa");
+    MouseUp(touch);
+  }
+}
+
+function posicionY(obj) {
+  var valor = obj.offsetTop;
+  if (obj.offsetParent) valor += posicionY(obj.offsetParent);
+  return valor;
+}
+
+function posicionX(obj) {
+  var valor = obj.offsetLeft;
+  if (obj.offsetParent) valor += posicionX(obj.offsetParent);
+  return valor;
+}
+
+/* Limpiar pizarra */
+function LimpiarTrazado(){
+  contexto=document.getElementById(idCanvas).getContext('2d');
+  contexto.fillStyle=colorDeFondo;
+  contexto.fillRect(0,0,anchoCanvas,altoCanvas);
+}
+
+let imagen_1=false;
+let imagen_2=true;
+let imagen_3=true;
+
+/* Enviar el trazado */
+function GuardarTrazado(){
+  aux=document.getElementById(idCanvas).toDataURL('image/png');
+  var img = document.querySelector('#img1');
+  var img2 = document.querySelector('#img2');
+  var img3 = document.querySelector('#img3');
+  if(imagen_3==false){
+    imagen_3=true;
+    img3.src=aux;
+    imagen3.value=aux;
+    LimpiarTrazado()
+    document.forms[idForm].submit();
+  }
+  if(imagen_2==false){
+    imagen_2=true;
+    imagen_3=false;
+    img2.src=aux;
+    imagen2.value=aux
+    LimpiarTrazado()
+  }
+  if(imagen_1==false){
+    imagen_1=true;
+    imagen_2=false;
+    img.src=aux;
+    imagen1.value=aux;
+    LimpiarTrazado()
+  }
+  
+  
+  
+}
+</script>
+
+<?php 
+
+// comprovamos si se envió la imagen
+if (isset($_POST['imagen1'])) { 
+
+    // funcion para gusrfdar la imagen base64 en el servidor
+    // el nombre debe tener la extension
+    function uploadImgBase64 ($base64, $name){
+        // decodificamos el base64
+        $datosBase64 = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64));
+        // definimos la ruta donde se guardara en el server
+        $path= 'upload/firmas/'.$name;
+
+        // guardamos la imagen en el server
+        if(!file_put_contents($path, $datosBase64)){
+            // retorno si falla
+            echo $path;
+            return false;
+        }
+        else{
+            // retorno si todo fue bien
+            return true;
+        }
+    }
+
+    // llamamos a la funcion uploadImgBase64( img_base64, nombre_fina.png) 
+    for ($i=1;$i<=3;$i++){
+      uploadImgBase64($_POST['imagen'.$i.''], '1007706733firma'.$i.'_'.date('d_m_Y_H_i_s').'.png' );
+    }
+}
+?>
